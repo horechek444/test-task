ab2b3b2bf1cc714d77cf9e661fd3824d012e4a0d
Tatiana Eskina <horechek444@gmail.com>
2021-03-20 18:19:45 +0300

    feat: подготовка  инпутов

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,36 @@
 <template>
   <div class="container">
-    <h1 class="title">Статьи</h1>
-    <ul class="list">
+    <h1 class="container__title">Статьи</h1>
+    <ul class="container__list">
+      <li class="container__item item" v-for="article in articles" :key="article._id">
+        <h2 class="item__title">{{ article.title }}</h2>
+        <p class="item__text">{{ article.text }}</p>
+      </li>
     </ul>
+    <form class="form"
+          action="#"
+          name="edit"
+          @submit="handleSubmit"
+          noValidate
+    >
+      <label class="control">
+        <input
+          class="input input__title"
+          type="text" name="title"
+          v-model="newTitle"
+          @change="handleInputChange" placeholder="Заголовок статьи" minLength="2" maxLength="20"
+          required/>
+      </label>
+      <label class="control">
+        <textarea
+          class="input input__text" name="text" v-model="newText"
+          @change="handleInputChange" placeholder="Текст статьи" minLength="2" maxLength="200" required />
+      </label>
+      <input
+        class="submit"
+        type="submit" value="Сохранить" name="submit"
+      />
+    </form>
   </div>
 </template>
 
@@ -10,42 +38,58 @@
 import Api from '@/services/api'
 
 export default {
-  data() {
+  data () {
     return {
-      articles: [],
+      articles: null,
+      newTitle: "",
+      newText: ""
     }
   },
-  created() {
-    this.getArticles();
+  created () {
+    this.getArticles()
   },
   methods: {
-    async getArticles() {
+    async getArticles () {
       try {
-        this.articles.push(await Api.getArticles());
+        this.articles = await Api.getArticles()
       } catch (err) {
-        console.log(`${err}`);
+        console.log(`${err}`)
       }
     },
-    handleArticleCreate(inputValue) {
+    handleArticleCreate (inputValue) {
       Api.createArticle(inputValue)
         .then((newArticle) => {
-          this.articles.push(newArticle);
+          this.articles = [...this.articles, newArticle]
         })
         .catch((err) => {
-          console.log(`${err}`);
+          console.log(`${err}`)
         })
     },
-    handleArticleDelete(article) {
+    handleSubmit () {
+      let title = this.newTitle && this.newTitle.trim();
+      let text = this.newText && this.newText.trim();
+      if (!title && !text) {
+        return;
+      }
+      let inputValue = {
+        title: title,
+        text: text
+      };
+      this.handleArticleCreate(inputValue);
+      this.newTitle = "";
+      this.newText = "";
+    },
+    handleArticleDelete (article) {
       Api.deleteArticle(article._id)
         .then(() => {
-          this.articles = this.articles.filter((a) => a._id !== article._id);
+          this.articles = this.articles.filter((a) => a._id !== article._id)
         })
         .catch((err) => {
-          console.log(`${err}`);
+          console.log(`${err}`)
         })
     },
   },
-};
+}
 </script>
 
 <style>
@@ -53,16 +97,35 @@ export default {
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
   color: #35495e;
 }
 
-.list {
-  list-style: none;
+.container__title {
+  margin-bottom: 20px;
 }
+
+.container__list {
+  list-style: none;
+  padding: 0;
+}
+
+.container__item {
+  min-width: 200px;
+  min-height: 70px;
+  color: #3b8070;
+}
+
+.container__item:not(:last-of-type) {
+  margin-bottom: 10px;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+}
+
 </style>
